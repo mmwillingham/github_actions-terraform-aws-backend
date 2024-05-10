1ca28a743db4d39e3ffac244a7e35ea098f7ac8e
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-10 09:36:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,59 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
+provider "aws" {
+  region     = "${var.aws_region}"
+}
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  
-  backend "s3" {
-    bucket         	   = "bl-terraform-tfstate"
-    key              	   = "state/terraform.tfstate"
-    region         	   = "us-east-2"
-    encrypt        	   = true
-    dynamodb_table = "bl-terraform-tfstate-lockid"
+#### S3
+resource "aws_s3_bucket" "s3_bucket_backend" {
+  bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_backend_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+  bucket     = aws_s3_bucket.s3_bucket_backend.id
+  acl        = "private"
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.s3_bucket_backend.id
+  rule {
+    object_ownership = "ObjectWriter"
   }
 }
 
-# Configure and downloading plugins for aws
-provider "aws" {
-  region     = "${var.aws_region}"
+resource "aws_s3_bucket_versioning" "s3_bucket_version" {
+  bucket = aws_s3_bucket.s3_bucket_backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "terraform_folder" {
+  bucket = aws_s3_bucket.s3_bucket_backend.id
+  key    = "terraform.tfstate"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
+  bucket                  = aws_s3_bucket.s3_bucket_backend.bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#### DynamoDB
+
+resource "aws_dynamodb_table" "dynamodb_tfstate_lock" {
+  name           = "tfstate-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
 }
