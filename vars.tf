ef1381ad88793554c4e7fe7a6b24919ed9a81365
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-15 15:06:57 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -4,10 +4,10 @@ variable "aws_region" {
 
 variable "s3_bucket_name" {
   type    = string
-  default = "bolauder-tfstate2"
+  default = "rosa-tfstate"
 }
 
 variable "dynamoDB_table_name" {
   type    = string
-  default = "bolauder-tfstate2"
+  default = "rosa-tfstate"
 }
