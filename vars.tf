d96016fb609c89ab79a146e64a3e5455bd209120
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-20 14:16:27 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -4,10 +4,10 @@ variable "aws_region" {
 
 variable "s3_bucket_name" {
   type    = string
-  default = "ec2-tfstate-20240517"
+  default = "rosa-tfstate-20240520"
 }
 
 variable "dynamoDB_table_name" {
   type    = string
-  default = "ec2-tfstate-20240517"
+  default = "rosa-tfstate-20240520"
 }
